0938016f7cf296326b14a17241c61f309b6ffd3e
AdinaPirjol <adinapirjol@gmail.com>
2015-01-12 21:01:08 +0200

    Added checking for network connection and query for finding user credentials

--- app/src/main/java/com/app/md_hw/LoginActivity.java
+++ app/src/main/java/com/app/md_hw/LoginActivity.java
@@ -1,5 +1,9 @@
 package com.app.md_hw;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,8 +12,16 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -20,6 +32,7 @@ public class LoginActivity extends ActionBarActivity {
         setContentView(R.layout.activity_login);
 
         // Enable Local Datastore
+        // for when there is no available internet connection
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(this, "pizEYCj8zYu8dcQ9AU7DxfRqGjx6Lt9Ie2aHV8Rn", "5e9eoQeZiL8yKntAe9tRmRsp3Xe7l9fLShGwoz7E");
@@ -31,15 +44,37 @@ public class LoginActivity extends ActionBarActivity {
 
 
     private void login(View view){
-        EditText editUsername = (EditText) findViewById(R.id.editUsername);
-        EditText editPassword = (EditText) findViewById(R.id.editPassword);
+        if(isNetworkConnected()) {
+            EditText editUsername = (EditText) findViewById(R.id.editUsername);
+            EditText editPassword = (EditText) findViewById(R.id.editPassword);
 
-        String username = editUsername.getText().toString().trim();
-        String password = editPassword.getText().toString().trim();
+            String username = editUsername.getText().toString().trim();
+            String password = editPassword.getText().toString().trim();
 
-        if(username.isEmpty() || password.isEmpty()){
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this.getApplicationContext(), "Username and/or password are null!", Toast.LENGTH_SHORT);
+            } else {
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("UserCredentials");
+
+                ParseObject credentials = new ParseObject("UserCredentials");
+                credentials.add("username", username);
+                credentials.add("password", password);
+
+//                query.findInBackground();
+            }
+        } else {
             Toast.makeText(this.getApplicationContext(), "Username and/or password are null!", Toast.LENGTH_SHORT);
         }
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (info == null) {
+            // There are no active networks.
+            return false;
+        } else
+            return true;
     }
 
     @Override
@@ -53,7 +88,7 @@ public class LoginActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // as you specify a parent activity in AndroidMainfofest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
