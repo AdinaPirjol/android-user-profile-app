f2853b0e049534837e4af83ad00a8c2184e94d99
AdinaPirjol <adinapirjol@gmail.com>
2015-01-14 14:45:10 +0200

    Re-added checking for network connection when signing in

--- app/src/main/java/com/app/md_hw/LoginActivity.java
+++ app/src/main/java/com/app/md_hw/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.app.md_hw;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -53,72 +56,95 @@ public class LoginActivity extends Activity {
         loginbutton.setOnClickListener(new OnClickListener() {
 
             public void onClick(View arg0) {
-                // Retrieve the text entered from the EditText
-                usernametxt = username.getText().toString();
-                passwordtxt = password.getText().toString();
+                if(isNetworkConnected()) {
+                    // Retrieve the text entered from the EditText
+                    usernametxt = username.getText().toString();
+                    passwordtxt = password.getText().toString();
 
-                // Send data to Parse.com for verification
-                ParseUser.logInInBackground(usernametxt, passwordtxt,
-                        new LogInCallback() {
-                            public void done(ParseUser user, ParseException e) {
-                                if (user != null) {
-                                    // If user exist and authenticated, send user to HomeActivity
-                                    Intent intent = new Intent(
-                                            LoginActivity.this,
-                                            HomeActivity.class);
-                                    startActivity(intent);
-                                    Toast.makeText(getApplicationContext(),
-                                            "Successfully Logged in",
-                                            Toast.LENGTH_LONG).show();
-                                    finish();
-                                } else {
-                                    Toast.makeText(
-                                            getApplicationContext(),
-                                            "No such user exist, please signup",
-                                            Toast.LENGTH_LONG).show();
+                    // Send data to Parse.com for verification
+                    ParseUser.logInInBackground(usernametxt, passwordtxt,
+                            new LogInCallback() {
+                                public void done(ParseUser user, ParseException e) {
+                                    if (user != null) {
+                                        // If user exist and authenticated, send user to HomeActivity
+                                        Intent intent = new Intent(
+                                                LoginActivity.this,
+                                                HomeActivity.class);
+                                        startActivity(intent);
+                                        Toast.makeText(getApplicationContext(),
+                                                "Successfully Logged in",
+                                                Toast.LENGTH_LONG).show();
+                                        finish();
+                                    } else {
+                                        Toast.makeText(
+                                                getApplicationContext(),
+                                                "No such user exist, please signup",
+                                                Toast.LENGTH_LONG).show();
+                                    }
                                 }
-                            }
-                        });
+                            });
+                }else{
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Device is not connected to the internet. You cannot log in.",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
         // Sign up Button Click Listener
         signup.setOnClickListener(new OnClickListener() {
 
             public void onClick(View arg0) {
-                // Retrieve the text entered from the EditText
-                usernametxt = username.getText().toString();
-                passwordtxt = password.getText().toString();
+                if(isNetworkConnected()) {
+                    // Retrieve the text entered from the EditText
+                    usernametxt = username.getText().toString();
+                    passwordtxt = password.getText().toString();
 
-                // Force user to fill up the form
-                if (usernametxt.equals("") && passwordtxt.equals("")) {
-                    Toast.makeText(getApplicationContext(),
-                            "Please complete the sign up form",
-                            Toast.LENGTH_LONG).show();
+                    // Force user to fill up the form
+                    if (usernametxt.equals("") && passwordtxt.equals("")) {
+                        Toast.makeText(getApplicationContext(),
+                                "Please complete the sign up form",
+                                Toast.LENGTH_LONG).show();
 
-                } else {
-                    // Save new user data into Parse.com Data Storage
-                    ParseUser user = new ParseUser();
-                    user.setUsername(usernametxt);
-                    user.setPassword(passwordtxt);
-                    user.signUpInBackground(new SignUpCallback() {
-                        public void done(ParseException e) {
-                            if (e == null) {
-                                // Show a simple Toast message upon successful registration
-                                Toast.makeText(getApplicationContext(),
-                                        "Successfully Signed up, please log in.",
-                                        Toast.LENGTH_LONG).show();
-                            } else {
-                                Toast.makeText(getApplicationContext(),
-                                        "Sign up Error", Toast.LENGTH_LONG)
-                                        .show();
+                    } else {
+                        // Save new user data into Parse.com Data Storage
+                        ParseUser user = new ParseUser();
+                        user.setUsername(usernametxt);
+                        user.setPassword(passwordtxt);
+                        user.signUpInBackground(new SignUpCallback() {
+                            public void done(ParseException e) {
+                                if (e == null) {
+                                    // Show a simple Toast message upon successful registration
+                                    Toast.makeText(getApplicationContext(),
+                                            "Successfully Signed up, please log in.",
+                                            Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(),
+                                            "Sign up Error", Toast.LENGTH_LONG)
+                                            .show();
+                                }
                             }
-                        }
-                    });
+                        });
+                    }
+                }else{
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Device is not connected to the internet. You cannot log in.",
+                            Toast.LENGTH_LONG).show();
                 }
-
             }
         });
 
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (info == null) {
+            // There are no active networks.
+            return false;
+        } else
+            return true;
     }
 }
 
