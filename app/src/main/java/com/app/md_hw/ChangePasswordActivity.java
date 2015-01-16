89200f5915cad0623d5523848b61cda63fbc1e69
codreanulaurentiu <lauwrestler@yahoo.co.uk>
2015-01-16 22:22:08 +0200

    more comments

--- app/src/main/java/com/app/md_hw/ChangePasswordActivity.java
+++ app/src/main/java/com/app/md_hw/ChangePasswordActivity.java
@@ -25,6 +25,7 @@ import com.parse.ParseUser;
  * Created by Lau on 13.01.2015.
  */
 public class ChangePasswordActivity extends Activity {
+    //create 2 edittext fields and a new button
     private EditText editNew;
     private EditText editRetype;
     private Button buttonSaveNewPassword;
@@ -32,6 +33,7 @@ public class ChangePasswordActivity extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set the layout from activity_change_password
         setContentView(R.layout.activity_change_password);
 
         editNew = (EditText) findViewById(R.id.editNew);
@@ -46,10 +48,11 @@ public class ChangePasswordActivity extends Activity {
                 retypeP = editRetype.getText().toString().trim();
 
                 ParseUser currentUser = ParseUser.getCurrentUser();
-
+                //if either field is empty return an error
                 if(newP == "" || retypeP == ""){
                     Toast.makeText(getApplicationContext(), "You didn't complete all fields", Toast.LENGTH_LONG).show();
                 }else{
+                //if they are correctly set the new one
                     if (newP.compareTo(retypeP) != 0) {
                         Toast.makeText(getApplicationContext(), "New password and retype new password do not match!", Toast.LENGTH_LONG).show();
                     } else {
